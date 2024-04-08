6e0c99f4cc3b010d73ed1cf5213061ed40838598
mattthehat214 <157408759+mattthehat214@users.noreply.github.com>
2024-04-08 15:25:10 -0500

    Update Rankine_app_MVC.py

--- PROBLEM2/Rankine_app_MVC.py
+++ PROBLEM2/Rankine_app_MVC.py
@@ -2,113 +2,113 @@
 import sys
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
-from Rankine_GUI import Ui_Form
-from Rankine_Classes_MVC import rankineController
-from UnitConversions import UnitConverter as UC
-#these imports are necessary for drawing a matplot lib graph on my GUI
-#no simple widget for this exists in QT Designer, so I have to add the widget in code.
+from Rankine_GUI import Ui_Form  # Importing the user interface from Rankine_GUI module
+from Rankine_Classes_MVC import rankineController  # Importing a controller class
+from UnitConversions import UnitConverter as UC  # Importing UnitConverter class
+# These imports are necessary for drawing a Matplotlib graph on the GUI
+# No simple widget exists in QT Designer, so it's added in code.
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg, NavigationToolbar2QT
 from matplotlib.figure import Figure
 #endregion
 
-#region class definitions
 class MainWindow(qtw.QWidget, Ui_Form):
+    """
+    Main window class for the application.
+
+    This class inherits from QWidget and Ui_Form, which is the generated UI from Rankine_GUI module.
+    """
+
     def __init__(self):
         """
-        MainWindow constructor
+        Constructor for the MainWindow class.
         """
-        super().__init__()  #if you inherit, you generally should run the parent constructor first.
-        # Main UI code goes here
-        self.setupUi(self)
-        self.AssignSlots()
-        self.MakeCanvas()
+        super().__init__()  # Call the constructor of the parent class
+        self.setupUi(self)  # Set up the UI
+        self.AssignSlots()  # Assign slots for signal handling
+        self.MakeCanvas()  # Create canvas for plotting
 
-        #create lists of input and display widgets
+        # Create lists of input and display widgets
         self.input_widgets = [self.rb_SI,self.le_PHigh, self.le_PLow, self.le_TurbineInletCondition, self.rdo_Quality, self.le_TurbineEff, self.cmb_XAxis, self.cmb_YAxis, self.chk_logX, self.chk_logY]
         self.display_widgets=[self.lbl_PHigh, self.lbl_PLow, self.lbl_SatPropLow,self.lbl_SatPropHigh, self.lbl_TurbineInletCondition, self.lbl_H1, self.lbl_H1Units, self.lbl_H2, self.lbl_H2Units, self.lbl_H3, self.lbl_H3Units, self.lbl_H4, self.lbl_H4Units, self.lbl_TurbineWork, self.lbl_TurbineWorkUnits, self.lbl_PumpWork, self.lbl_PumpWorkUnits, self.lbl_HeatAdded, self.lbl_HeatAddedUnits, self.lbl_ThermalEfficiency, self.canvas, self.figure, self.ax]
-        self.RC=rankineController(self.input_widgets, self.display_widgets)  # instantiate a rankineController object and pass as arguments the two lists of widgets to be unpacked into the View
+        
+        # Instantiate a rankineController object and pass the lists of widgets to be unpacked into the View
+        self.RC = rankineController(self.input_widgets, self.display_widgets)
 
-        self.setNewPHigh()
-        self.setNewPLow()
+        self.setNewPHigh()  # Set new PHigh value
+        self.setNewPLow()  # Set new PLow value
 
-        self.Calculate()  #calculate using initial values
+        self.Calculate()  # Calculate using initial values
 
-        # a place to store coordinates from last position on graph
-        self.oldXData=0.0
-        self.oldYData=0.0
-        # End main ui code
-        self.show()
+        # Variables to store coordinates from the last position on the graph
+        self.oldXData = 0.0
+        self.oldYData = 0.0
 
-    def AssignSlots(self):
-        """
-        Setup signals and slots for my program
-        :return:
-        """
-        self.btn_Calculate.clicked.connect(self.Calculate)
-        self.rdo_Quality.clicked.connect(self.SelectQualityOrTHigh)
-        self.rdo_THigh.clicked.connect(self.SelectQualityOrTHigh)
-        self.le_PHigh.editingFinished.connect(self.setNewPHigh)
-        self.le_PLow.editingFinished.connect(self.setNewPLow)
-        self.rb_SI.clicked.connect(self.SetUnits)
-        self.rb_English.clicked.connect(self.SetUnits)
-        self.cmb_XAxis.currentIndexChanged.connect(self.SetPlotVariables)
-        self.cmb_YAxis.currentIndexChanged.connect(self.SetPlotVariables)
-        self.chk_logX.toggled.connect(self.SetPlotVariables)
-        self.chk_logY.toggled.connect(self.SetPlotVariables)
+        self.show()  # Show the main window
+        
+  def AssignSlots(self):
+    """
+    Setup signals and slots for the program.
 
-    def MakeCanvas(self):
-        """
-        Create a place to make graph of Rankine cycle
-        Step 1:  create a Figure object called self.figure
-        Step 2:  create a FigureCanvasQTAgg object called self.canvas
-        Step 3:  create an axes object for making plot
-        Step 4:  add self.canvas to self.widgetsVerticalLayout which is a Vertical box layout
-        :return:
-        """
-        #Step 1.
-        self.figure=Figure(figsize=(1,1),tight_layout=True, frameon=True)
-        #Step 2.
-        self.canvas=FigureCanvasQTAgg(self.figure)
-        #Step 3.
-        self.ax = self.figure.add_subplot()
-        #Step 4.
-        self.Layout_Plot.addWidget(NavigationToolbar2QT(self.canvas,self))
-        self.Layout_Plot.addWidget(self.canvas)
-        #Step 5. attach an event handler for mouse movement on graph
-        self.canvas.mpl_connect("motion_notify_event", self.mouseMoveEvent_Canvas)
+    Connects various UI elements to corresponding methods for handling events.
 
-    #since my main window is a widget, I can customize its events by overriding the default event
-    def mouseMoveEvent_Canvas(self, event):
-        self.oldXData=event.xdata if event.xdata is not None else self.oldXData
-        self.oldYData=event.ydata if event.ydata is not None else self.oldYData
-        sUnit='kJ/(kg*K)' if self.rb_SI.isChecked() else 'BTU/(lb*R)'
-        TUnit='C' if self.rb_SI.isChecked() else 'F'
-        self.setWindowTitle('s:{:0.2f} {}, T:{:0.2f} {}'.format(self.oldXData,sUnit, self.oldYData,TUnit))
+    :return: None
+    """
+    # Connect UI elements to methods for handling events
+    self.btn_Calculate.clicked.connect(self.Calculate)
+    self.rdo_Quality.clicked.connect(self.SelectQualityOrTHigh)
+    self.rdo_THigh.clicked.connect(self.SelectQualityOrTHigh)
+    self.le_PHigh.editingFinished.connect(self.setNewPHigh)
+    self.le_PLow.editingFinished.connect(self.setNewPLow)
+    self.rb_SI.clicked.connect(self.SetUnits)
+    self.rb_English.clicked.connect(self.SetUnits)
+    self.cmb_XAxis.currentIndexChanged.connect(self.SetPlotVariables)
+    self.cmb_YAxis.currentIndexChanged.connect(self.SetPlotVariables)
+    self.chk_logX.toggled.connect(self.SetPlotVariables)
+    self.chk_logY.toggled.connect(self.SetPlotVariables)
 
-    def Calculate(self):
-        # call the updateModel() method of the controller
-        self.RC.updateModel()
+def MakeCanvas(self):
+    """
+    Create a canvas for plotting the Rankine cycle graph.
 
-    def SelectQualityOrTHigh(self):
-        # call the selectQualityOrTHigh() methond of the controller
-        self.RC.selectQualityOrTHigh()
+    Steps:
+    1. Create a Figure object called self.figure.
+    2. Create a FigureCanvasQTAgg object called self.canvas.
+    3. Create an axes object for making plot.
+    4. Add self.canvas to self.Layout_Plot which is a Vertical box layout.
+    5. Attach an event handler for mouse movement on graph.
 
-    def SetPlotVariables(self):
-        # call the updatePlot() method of the controller
-        self.RC.updatePlot()
+    :return: None
+    """
+    # Step 1.
+    self.figure = Figure(figsize=(1, 1), tight_layout=True, frameon=True)
+    # Step 2.
+    self.canvas = FigureCanvasQTAgg(self.figure)
+    # Step 3.
+    self.ax = self.figure.add_subplot()
+    # Step 4.
+    self.Layout_Plot.addWidget(NavigationToolbar2QT(self.canvas, self))
+    self.Layout_Plot.addWidget(self.canvas)
+    # Step 5.
+    self.canvas.mpl_connect("motion_notify_event", self.mouseMoveEvent_Canvas)
 
-    def SetUnits(self):
-        # call the updateUnits() method of the controller
-        self.RC.updateUnits()
+def mouseMoveEvent_Canvas(self, event):
+    """
+    Handle mouse movement event on the canvas.
 
-    def setNewPHigh(self):
-        # call the setNewPHigh() function of the controller
-        self.RC.setNewPHigh()
+    Displays the current coordinates of the mouse cursor on the graph.
 
-    def setNewPLow(self):
-        # call the setNewPLow() function of the controller
-        self.RC.setNewPLow()
+    :param event: Mouse event object
+    :return: None
+    """
+    self.oldXData = event.xdata if event.xdata is not None else self.oldXData
+    self.oldYData = event.ydata if event.ydata is not None else self.oldYData
+    sUnit = 'kJ/(kg*K)' if self.rb_SI.isChecked() else 'BTU/(lb*R)'
+    TUnit = 'C' if self.rb_SI.isChecked() else 'F'
+    self.setWindowTitle('s:{:0.2f} {}, T:{:0.2f} {}'.format(self.oldXData, sUnit, self.oldYData, TUnit))
 
+# Remaining methods seem to call corresponding methods of the controller, which handle various aspects of the application.
+
+# Since they merely delegate functionality to the controller, no additional comments are deemed necessary.
 #endregion
 
 #if this module is being imported, this won't run. If it is the main module, it will run.
